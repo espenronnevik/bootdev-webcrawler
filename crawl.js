99895b14c6d9abdd846a08a8dd1f17a14e480a0a
Espen Rønnevik <espen@digitalsoup.eu>
2024-07-21 11:58:26 +0200

    Implement normalizeURL that passes tests, and some more tests from the webside

--- crawl.js
+++ crawl.js
@@ -1,5 +1,10 @@
 function normalizeURL(url) {
-  return url;
+  const normURL = new URL(url);
+  let newURL = `${normURL.host}${normURL.pathname}`;
+  if (newURL.endsWith("/")) {
+    newURL = newURL.slice(0, -1);
+  }
+  return newURL;
 }
 
 export { normalizeURL };
--- crawl.test.js
+++ crawl.test.js
@@ -6,3 +6,15 @@ test("https url normalization 1", () => {
     "blog.boot.dev/path",
   );
 });
+
+test("https url normalization 2", () => {
+  expect(normalizeURL("https://blog.boot.dev/path")).toBe("blog.boot.dev/path");
+});
+
+test("https url normalization 3", () => {
+  expect(normalizeURL("http://blog.boot.dev/path/")).toBe("blog.boot.dev/path");
+});
+
+test("https url normalization 4", () => {
+  expect(normalizeURL("http://blog.boot.dev/path")).toBe("blog.boot.dev/path");
+});
